d5dfbabc08f62bd3ac2a1f63fc100a143942fad2
Mariana Bassani Heleno <happiny.kawaii@gmail.com>
2020-09-25 21:36:29 -0300

    codiguin maroto

---
+++ Projetos/JogoForca/JavaScript/script.js
@@ -0,0 +1,27 @@
+var palavra = new Array();
+function jogar(){
+
+    palavra[0] = "r";
+    palavra[1] = "a";
+    palavra[2] = "t";
+    palavra[3] = "o";
+    verificaPalavra();
+
+}
+function verificaPalavra(){
+    var letra = prompt("Digite aqui sua letra");
+    for( var i = 0; i < palavra.length; i++)
+    {
+        if(palavra[i] == letra)
+        {
+            alert("Você acertou!");
+        }
+        else
+        {
+            alert("Você errou, forca!");
+     
+        }
+        
+    }
+
+}

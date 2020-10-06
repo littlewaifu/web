f42bdf2c63dd90a28f4b981ce96dc3ab2857c384
MatheusBraga <matheusbrag22@gmail.com>
2020-10-06 00:14:18 -0300

    Nomes e Escolha

--- Projetos/JogoDaVelha/JS/script.js
+++ Projetos/JogoDaVelha/JS/script.js
@@ -5,7 +5,37 @@ var fimJogo = false;
 var imgX = "Image/x-jogo-velha.jpg";
 var imgO = "Image/o-jogo-velha.jpg";
 
+var escolhaXouO;
 
+function novoJogo()
+{
+    var nomeP1 = prompt('Digite o nome do Jogador 1');
+    var nomeP2 = prompt('Digite o nome do Jogador 2');
+    document.getElementById('jogadorAtual').innerHTML = "Vez do: " + nomeP1;
+
+    escolhaXouO = prompt(nomeP1 + ", agora digite X ou O, para escolher seu símbolo");
+
+    if (escolhaXouO == null || escolhaXouO == '' || (escolhaXouO != "x" && escolhaXouO != "o"))
+    {
+        alert(nomeP1 + "Você deve digitar x ou o para prosseguir");
+        location.reload();
+    }
+
+    var imgJogador = document.getElementById("jogadorAtualImg");
+    imgJogador.src = imgEscolha();
+}
+
+function imgEscolha()
+{
+    if (escolhaXouO == "x")
+    {
+        return imgX;
+    }
+    if (escolhaXouO == "o")
+    {
+        return imgO;
+    }
+}
 
 function mostraVez() {
     if (fimJogo == true) {
@@ -13,24 +43,24 @@ function mostraVez() {
     }
 
     if (vezJogador == jogador1) {
-        var imgJogador = document.getElementById("proxJogador");
+        var imgJogador = document.getElementById("jogadorAtualImg");
         imgJogador.src = imgX;
     }
     else {
-        var imgJogador = document.getElementById("proxJogador");
+        var imgJogador = document.getElementById("jogadorAtualImg");
         imgJogador.src = imgO;
     }
 
 
 }
 
-function jogada(ident){
-    if(vezJogador == jogador1){
+function jogada(ident) {
+    if (vezJogador == jogador1) {
         var imgJogada = document.getElementById(ident);
         imgJogada.src = imgX;
         vezJogador = jogador2;
         mostraVez();
-    }else{
+    } else {
         var imgJogada = document.getElementById(ident);
         imgJogada.src = imgO;
         vezJogador = jogador1;

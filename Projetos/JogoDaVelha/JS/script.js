556e165de97c2ba74940e0109df8de2e06d94bfb
MatheusBraga <matheusbrag22@gmail.com>
2020-10-06 19:17:46 -0300

    .

--- Projetos/JogoDaVelha/JS/script.js
+++ Projetos/JogoDaVelha/JS/script.js
@@ -1,38 +1,46 @@
-var jogador1 = "x";
-var jogador2 = "o";
-var vezJogador = jogador1;
+var imgP1;
+var imgP2;
+var vezJogador;
 var fimJogo = false;
 var imgX = "Image/x-jogo-velha.jpg";
 var imgO = "Image/o-jogo-velha.jpg";
 
+var nomeP1;
+var nomeP2;
 var escolhaXouO;
 
-function novoJogo()
-{
-    var nomeP1 = prompt('Digite o nome do Jogador 1');
-    var nomeP2 = prompt('Digite o nome do Jogador 2');
+function novoJogo() {
+    nomeP1 = prompt('Digite o nome do Jogador 1');
+    nomeP2 = prompt('Digite o nome do Jogador 2');
     document.getElementById('jogadorAtual').innerHTML = "Vez do: " + nomeP1;
 
     escolhaXouO = prompt(nomeP1 + ", agora digite X ou O, para escolher seu símbolo");
 
-    if (escolhaXouO == null || escolhaXouO == '' || (escolhaXouO != "x" && escolhaXouO != "o"))
-    {
+    if (escolhaXouO == null || escolhaXouO == '' || (escolhaXouO != "x" && escolhaXouO != "o")) {
         alert(nomeP1 + "Você deve digitar x ou o para prosseguir");
         location.reload();
     }
 
     var imgJogador = document.getElementById("jogadorAtualImg");
     imgJogador.src = imgEscolha();
+
+    if (imgEscolha() == imgX) {
+        imgP1 = imgX;
+        imgP2 = imgO;
+    }
+    else {
+        imgP1 = imgO;
+        imgP2 = imgX;
+    }
+
+    vezJogador = nomeP1;
 }
 
-function imgEscolha()
-{
-    if (escolhaXouO == "x")
-    {
+function imgEscolha() {
+    if (escolhaXouO == "x") {
         return imgX;
     }
-    if (escolhaXouO == "o")
-    {
+    if (escolhaXouO == "o") {
         return imgO;
     }
 }
@@ -42,51 +50,26 @@ function mostraVez() {
         return;
     }
 
-    if (vezJogador == jogador1) {
+    if (vezJogador == nomeP1) {
         var imgJogador = document.getElementById("jogadorAtualImg");
-        imgJogador.src = imgX;
+        imgJogador.src = imgP1;
     }
     else {
         var imgJogador = document.getElementById("jogadorAtualImg");
-        imgJogador.src = imgO;
+        imgJogador.src = imgP2;
     }
-
-
 }
 
 function jogada(ident) {
-    if (vezJogador == jogador1) {
+    if (vezJogador == nomeP1) {
         var imgJogada = document.getElementById(ident);
-        imgJogada.src = imgX;
-        vezJogador = jogador2;
+        imgJogada.src = imgP1;
+        vezJogador = nomeP2;
         mostraVez();
     } else {
         var imgJogada = document.getElementById(ident);
-        imgJogada.src = imgO;
-        vezJogador = jogador1;
+        imgJogada.src = imgP2;
+        vezJogador = nomeP1;
         mostraVez();
     }
 }
-
-/*function iniciarJogada() {
-    var casa = document.getElementsByClassName("quad");
-    for (var i = 0; i < casa.length; i++) {
-
-        casa[i].addEventListener("click", "jogada()")
-    }
-}*/
-
-/*function jogada() {
-    if (fimJogo == true) {
-        return;
-    }
-    if (this.getElementsByTagName("img").lenght == 0) {
-        if (vezJogador == jogador1) {
-            this.innerHTML = "<img src = 'Image/x-jogo-velha.jpg'>";
-        } else {
-            this.innerHTML = "<img src = 'Image/o-jogo-velha.jpg'>";
-        }
-    }
-
-}
-*/
